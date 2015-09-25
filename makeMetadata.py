4519e88986b9c06f5a2655589e1d54d1c32059d4
Aidan Harris <mail@aidanharris.io>
2015-09-25 20:26:00 +0100

    Fix spelling error in 'makeMetadata.py'

--- makeMetadata.py
+++ makeMetadata.py
@@ -5,12 +5,12 @@ if(len(sys.argv) < 2):
 output = ''
 output += '---\n'
 output += 'title: ' + sys.argv[1].split('/')[len(sys.argv[1].split('/'))-1].title() + '\n'
-output += 'author: ' + 'Kyle Simpsons' + '\n'
+output += 'author: ' + 'Kyle Simpson' + '\n'
 output += 'rights: ' + 'Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License' + '\n'
 output += 'language: ' + 'en-US' + '\n'
 output += '...'
 try:
-	if(sys.argv[3] == '--debug' or sys.argv[3] == 'd'):
+	if(sys.argv[3] == '--debug' or sys.argv[3] == '-d'):
 		print(output)
 except Exception as e:
 	pass
